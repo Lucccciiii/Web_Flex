2d0f5ff10a9065a8006e1444ccfdf5f4f4600d77
Luci <9022649@student.zadkine.nl>
2023-10-09 11:40:02 +0200

    Forum
    Added forum index and user settings.
    Also continued working on login.

--- login.php
+++ login.php
@@ -1,7 +1,20 @@
 <?php
+$servername = "localhost";
+$dbusername = "username";
+$dbpassword = "password";
+$dbname = "Flex_webDB";
 
-$username = "uname";
-$password = "psw";
+$username = $_POST["uname"];
+$password = $_POST["psw"];
+$passwordscram = "";
 
-echo $username;
-echo $password;
+$conn = new mysqli($servername, $dbusername, $dbname);
+
+if ($conn->connect_error){
+    die("Connection Failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO users(id,user,password)
+VALUES (0,$username,$passwordscram);";
+
+echo $username, " ", $password;
